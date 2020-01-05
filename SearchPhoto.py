404c639f22d8dc543d9b4a57a1a655b209573425
RostislavFisher <59475110+RostislavFisher@users.noreply.github.com>
2020-01-05 15:23:26 +0200

    Update SearchPhoto.py

--- SearchPhoto.py
+++ SearchPhoto.py
@@ -20,16 +20,16 @@ RU:Выберите сервис
 ChoiceService = input()
 if ChoiceService == "1":
     print("""
-    ENG: Choice type of file
-    UA:Оберіть тип файлу
-    RU:Выберите тип файла
+ENG: Choice type of file
+UA:Оберіть тип файлу
+RU:Выберите тип файла
 
-    =====================
-    [1]ico
-    [2]png
-    [3]jpg
-    [4]gif
-    [5]Another/Інше/Другое
+=====================
+[1]ico
+[2]png
+[3]jpg
+[4]gif
+[5]Another/Інше/Другое
     """)
     Choice = input()
     if Choice == "1":
@@ -83,9 +83,10 @@ if ChoiceService == "1":
                 ListWrite.close();
     if Choice == "5":
         print("""
-        ENG: Input your type
-        UA: Введіть свій тип
-        RU: Введите свой тип""")
+ENG: Input your type
+UA: Введіть свій тип
+RU: Введите свой тип
+        """)
         OwnType = input()
         while True:
             Adr = url + ''.join([random.choice(list(Key)) for x in range(7)]) + OwnType
@@ -106,25 +107,31 @@ if ChoiceService == "2":
     removedURL = "https://prnt.sc/18jgksdgh"
     RemovedReq = requests.get(removedURL, headers={'User-Agent': 'Mozilla/5.0'})
     RemovedReq = bs4.BeautifulSoup(RemovedReq.text, "html.parser")
-    print(RemovedReq)
     RemovedReq = RemovedReq.find('img', id='screenshot-image')['src']
-    while True:
+    print("""
+ENG: Choice file amount
+UA:Оберіть кількість файлів
+RU:Выберите количество файлов
+
+=====================
+    
+    """)
+    FileAmount = int(input())
+    FileLen = 0
+    while FileAmount > FileLen:
         Adr = url + ''.join([random.choice(list(Key)) for x in range(6)])
         
 
         RequestedImage = requests.get(Adr, headers={'User-Agent': 'Mozilla/5.0'})
         RequestedImage = bs4.BeautifulSoup(RequestedImage.text, "html.parser")
         RequestedImage = RequestedImage.find('img', id='screenshot-image')['src']
-        print(RequestedImage)
-        print(RemovedReq)
-        import time
         if RequestedImage != RemovedReq:
             ListWrite = open("List.txt", 'a')
-            print(Adr + "########")
+            print(Adr)
             ListWrite.write(Adr + "\n")
             LenTxt += 1
             
             ListWrite.close();
             RequestedImage = requests.get(RequestedImage, allow_redirects=True).content
             open(str(LenTxt)+".png", 'wb').write(RequestedImage)
-    
+            FileLen = FileLen + 1
