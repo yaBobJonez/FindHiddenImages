2560fc0343bca47a023d68811dd5f666f1885692
RostislavFisher <59475110+RostislavFisher@users.noreply.github.com>
2020-01-05 14:20:42 +0200

    Add files via upload
    Added Lightshot's parse

--- SearchPhoto.py
+++ SearchPhoto.py
@@ -1,84 +1,130 @@
+#####################################
+#                                   #
+#       Autor:RostislavFisher       #
+#       Country:Ukraine             #
+#####################################
 import requests
 import random
-Key = '123456789qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM'
-url = 'https://i.imgur.com/'
-removedURL = "https://i.imgur.com/removed.png"
-RemovedReq = requests.get(removedURL, allow_redirects=True).content
-LenTxt = len(open("List.txt", 'r').readlines())
-
 
 print("""
-ENG: Choice type of file
-UA:Оберіть тип файлу
-RU:Выберите тип файла
+ENG: Choice Service
+UA:Оберіть сервіс
+RU:Выберите сервис
 
 =====================
-[1]ico
-[2]png
-[3]jpg
-[4]gif
-[5]Another/Інше/Другое
+[1]Imgur
+[2]LightShot
+[3]
 """)
-Choice = input()
-if Choice == "1":
-    while True:
-        Adr = url + ''.join([random.choice(list(Key)) for x in range(7)]) + ".ico"
-        RequestedImage = requests.get(Adr, allow_redirects=True).content
-        if RequestedImage != RemovedReq:
-            ListWrite = open("List.txt", 'a')
-            print(Adr)
-            open(str(LenTxt)+".ico", 'wb').write(RequestedImage)
-            ListWrite.write(Adr + "\n")
-            LenTxt += 1
-            ListWrite.close();
-if Choice == "2":
-    while True:
-        Adr = url + ''.join([random.choice(list(Key)) for x in range(7)]) + ".png"
-        RequestedImage = requests.get(Adr, allow_redirects=True).content
-        if RequestedImage != RemovedReq:
-            ListWrite = open("List.txt", 'a')
-            print(Adr)
-            open(str(LenTxt)+".png", 'wb').write(RequestedImage)
-            ListWrite.write(Adr + "\n")
-            LenTxt += 1
-            ListWrite.close();
-if Choice == "3":
-    while True:
-        Adr = url + ''.join([random.choice(list(Key)) for x in range(7)]) + ".jpg"
-        RequestedImage = requests.get(Adr, allow_redirects=True).content
-        if RequestedImage != RemovedReq:
-            ListWrite = open("List.txt", 'a')
-            print(Adr)
-            open(str(LenTxt)+".jpg", 'wb').write(RequestedImage)
-            ListWrite.write(Adr + "\n")
-            LenTxt += 1
-            ListWrite.close();
-if Choice == "4":
-    while True:
-        Adr = url + ''.join([random.choice(list(Key)) for x in range(7)]) + ".gif"
-        RequestedImage = requests.get(Adr, allow_redirects=True).content
-        if RequestedImage != RemovedReq:
-            ListWrite = open("List.txt", 'a')
-            print(Adr)
-            open(str(LenTxt)+".gif", 'wb').write(RequestedImage)
-            ListWrite.write(Adr + "\n")
-            LenTxt += 1
-            ListWrite.close();
-if Choice == "5":
+
+ChoiceService = input()
+if ChoiceService == "1":
     print("""
-    ENG: Input your type
-    UA: Введіть свій тип
-    RU: Введите свой тип""")
-    OwnType = input()
+    ENG: Choice type of file
+    UA:Оберіть тип файлу
+    RU:Выберите тип файла
+
+    =====================
+    [1]ico
+    [2]png
+    [3]jpg
+    [4]gif
+    [5]Another/Інше/Другое
+    """)
+    Choice = input()
+    if Choice == "1":
+        Key = '123456789qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM'
+        url = 'https://i.imgur.com/'
+        removedURL = "https://i.imgur.com/removed.png"
+        RemovedReq = requests.get(removedURL, allow_redirects=True).content
+        LenTxt = len(open("List.txt", 'r').readlines())
+        while True:
+            Adr = url + ''.join([random.choice(list(Key)) for x in range(7)]) + ".ico"
+            RequestedImage = requests.get(Adr, allow_redirects=True).content
+            if RequestedImage != RemovedReq:
+                ListWrite = open("List.txt", 'a')
+                print(Adr)
+                open(str(LenTxt)+".ico", 'wb').write(RequestedImage)
+                ListWrite.write(Adr + "\n")
+                LenTxt += 1
+                ListWrite.close();
+    if Choice == "2":
+        while True:
+            Adr = url + ''.join([random.choice(list(Key)) for x in range(7)]) + ".png"
+            RequestedImage = requests.get(Adr, allow_redirects=True).content
+            if RequestedImage != RemovedReq:
+                ListWrite = open("List.txt", 'a')
+                print(Adr)
+                open(str(LenTxt)+".png", 'wb').write(RequestedImage)
+                ListWrite.write(Adr + "\n")
+                LenTxt += 1
+                ListWrite.close();
+    if Choice == "3":
+        while True:
+            Adr = url + ''.join([random.choice(list(Key)) for x in range(7)]) + ".jpg"
+            RequestedImage = requests.get(Adr, allow_redirects=True).content
+            if RequestedImage != RemovedReq:
+                ListWrite = open("List.txt", 'a')
+                print(Adr)
+                open(str(LenTxt)+".jpg", 'wb').write(RequestedImage)
+                ListWrite.write(Adr + "\n")
+                LenTxt += 1
+                ListWrite.close();
+    if Choice == "4":
+        while True:
+            Adr = url + ''.join([random.choice(list(Key)) for x in range(7)]) + ".gif"
+            RequestedImage = requests.get(Adr, allow_redirects=True).content
+            if RequestedImage != RemovedReq:
+                ListWrite = open("List.txt", 'a')
+                print(Adr)
+                open(str(LenTxt)+".gif", 'wb').write(RequestedImage)
+                ListWrite.write(Adr + "\n")
+                LenTxt += 1
+                ListWrite.close();
+    if Choice == "5":
+        print("""
+        ENG: Input your type
+        UA: Введіть свій тип
+        RU: Введите свой тип""")
+        OwnType = input()
+        while True:
+            Adr = url + ''.join([random.choice(list(Key)) for x in range(7)]) + OwnType
+            RequestedImage = requests.get(Adr, allow_redirects=True).content
+            if RequestedImage != RemovedReq:
+                ListWrite = open("List.txt", 'a')
+                print(Adr)
+                open(str(LenTxt)+OwnType, 'wb').write(RequestedImage)
+                ListWrite.write(Adr + "\n")
+                LenTxt += 1
+                ListWrite.close();
+if ChoiceService == "2":
+    import bs4
+    Key = '123456789qwertyuiopasdfghjklzxcvbnm'
+    url = 'https://prnt.sc/'
+    LenTxt = len(open("List.txt", 'r').readlines())
+    
+    removedURL = "https://prnt.sc/18jgksdgh"
+    RemovedReq = requests.get(removedURL, headers={'User-Agent': 'Mozilla/5.0'})
+    RemovedReq = bs4.BeautifulSoup(RemovedReq.text, "html.parser")
+    print(RemovedReq)
+    RemovedReq = RemovedReq.find('img', id='screenshot-image')['src']
     while True:
-        Adr = url + ''.join([random.choice(list(Key)) for x in range(7)]) + OwnType
-        RequestedImage = requests.get(Adr, allow_redirects=True).content
+        Adr = url + ''.join([random.choice(list(Key)) for x in range(6)])
+        
+
+        RequestedImage = requests.get(Adr, headers={'User-Agent': 'Mozilla/5.0'})
+        RequestedImage = bs4.BeautifulSoup(RequestedImage.text, "html.parser")
+        RequestedImage = RequestedImage.find('img', id='screenshot-image')['src']
+        print(RequestedImage)
+        print(RemovedReq)
+        import time
         if RequestedImage != RemovedReq:
             ListWrite = open("List.txt", 'a')
-            print(Adr)
-            open(str(LenTxt)+OwnType, 'wb').write(RequestedImage)
+            print(Adr + "########")
             ListWrite.write(Adr + "\n")
             LenTxt += 1
+            
             ListWrite.close();
-#r = requests.get(url, allow_redirects=True)
-#open('google.ico', 'wb').write(r.content)
+            RequestedImage = requests.get(RequestedImage, allow_redirects=True).content
+            open(str(LenTxt)+".png", 'wb').write(RequestedImage)
+    
