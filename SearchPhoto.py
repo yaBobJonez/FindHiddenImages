9be54e83f1b63ebd914cb410d03af5eb6fb512f6
yaBobJonez <yaBobJonez@gmail.com>
2020-09-03 17:21:26 +0300

    Refactored using DRY

--- SearchPhoto.py
+++ SearchPhoto.py
@@ -17,6 +17,24 @@ RU:Выберите сервис
 [3]
 """)
 
+Formats = {"1": ".ico", "2": ".png", "3": ".jpg", "4": ".gif"}
+def generate(Format):
+    Key = '123456789qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM'
+    url = 'https://i.imgur.com/'
+    removedURL = "https://i.imgur.com/removed.png"
+    RemovedReq = requests.get(removedURL, allow_redirects=True).content
+    LenTxt = len(open("List.txt", 'r').readlines())
+    while True:
+        Adr = url + ''.join([random.choice(list(Key)) for x in range(7)]) + Format
+        RequestedImage = requests.get(Adr, allow_redirects=True).content
+        if RequestedImage != RemovedReq:
+            ListWrite = open("List.txt", 'a')
+            print(Adr)
+            open(str(LenTxt)+Format, 'wb').write(RequestedImage)
+            ListWrite.write(Adr + "\n")
+            LenTxt += 1
+            ListWrite.close();
+
 ChoiceService = input()
 if ChoiceService == "1":
     print("""
@@ -32,55 +50,6 @@ RU:Выберите тип файла
 [5]Another/Інше/Другое
     """)
     Choice = input()
-    if Choice == "1":
-        Key = '123456789qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM'
-        url = 'https://i.imgur.com/'
-        removedURL = "https://i.imgur.com/removed.png"
-        RemovedReq = requests.get(removedURL, allow_redirects=True).content
-        LenTxt = len(open("List.txt", 'r').readlines())
-        while True:
-            Adr = url + ''.join([random.choice(list(Key)) for x in range(7)]) + ".ico"
-            RequestedImage = requests.get(Adr, allow_redirects=True).content
-            if RequestedImage != RemovedReq:
-                ListWrite = open("List.txt", 'a')
-                print(Adr)
-                open(str(LenTxt)+".ico", 'wb').write(RequestedImage)
-                ListWrite.write(Adr + "\n")
-                LenTxt += 1
-                ListWrite.close();
-    if Choice == "2":
-        while True:
-            Adr = url + ''.join([random.choice(list(Key)) for x in range(7)]) + ".png"
-            RequestedImage = requests.get(Adr, allow_redirects=True).content
-            if RequestedImage != RemovedReq:
-                ListWrite = open("List.txt", 'a')
-                print(Adr)
-                open(str(LenTxt)+".png", 'wb').write(RequestedImage)
-                ListWrite.write(Adr + "\n")
-                LenTxt += 1
-                ListWrite.close();
-    if Choice == "3":
-        while True:
-            Adr = url + ''.join([random.choice(list(Key)) for x in range(7)]) + ".jpg"
-            RequestedImage = requests.get(Adr, allow_redirects=True).content
-            if RequestedImage != RemovedReq:
-                ListWrite = open("List.txt", 'a')
-                print(Adr)
-                open(str(LenTxt)+".jpg", 'wb').write(RequestedImage)
-                ListWrite.write(Adr + "\n")
-                LenTxt += 1
-                ListWrite.close();
-    if Choice == "4":
-        while True:
-            Adr = url + ''.join([random.choice(list(Key)) for x in range(7)]) + ".gif"
-            RequestedImage = requests.get(Adr, allow_redirects=True).content
-            if RequestedImage != RemovedReq:
-                ListWrite = open("List.txt", 'a')
-                print(Adr)
-                open(str(LenTxt)+".gif", 'wb').write(RequestedImage)
-                ListWrite.write(Adr + "\n")
-                LenTxt += 1
-                ListWrite.close();
     if Choice == "5":
         print("""
 ENG: Input your type
@@ -88,16 +57,9 @@ UA: Введіть свій тип
 RU: Введите свой тип
         """)
         OwnType = input()
-        while True:
-            Adr = url + ''.join([random.choice(list(Key)) for x in range(7)]) + OwnType
-            RequestedImage = requests.get(Adr, allow_redirects=True).content
-            if RequestedImage != RemovedReq:
-                ListWrite = open("List.txt", 'a')
-                print(Adr)
-                open(str(LenTxt)+OwnType, 'wb').write(RequestedImage)
-                ListWrite.write(Adr + "\n")
-                LenTxt += 1
-                ListWrite.close();
+        generate("." + OwnType)
+    else:
+        generate(Formats[Choice])
 if ChoiceService == "2":
     import bs4
     Key = '123456789qwertyuiopasdfghjklzxcvbnm'
